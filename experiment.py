2bd9d5f0e238f20cc3b7189b65123283eed233ed
Hugo Huart <hugo.huart@heig-vd.ch>
2024-04-24 01:10:20 +0200

    Working simple full experiment

--- experiment.py
+++ experiment.py
@@ -1,27 +1,47 @@
-from llama_cpp import Llama
+from sklearn.metrics import accuracy_score, recall_score, f1_score
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+
+from tqdm import tqdm
 
 from pprint import pprint
 from tacos.data import DiscourseMTLoader
-from tacos.prompt import SimpleENFRPromptFormatter
 
 def simple_comparison_mistral():
     path = "discourse-mt-test-sets/test-sets/anaphora.json"
     testset = DiscourseMTLoader().comparison_from_json(path=path)
 
-    
-    model_path = "models/mistral/7b/ggml-model-Q4_K_M.gguf"
-    llm = Llama(model_path=model_path)
-
-    prompt_fmt = SimpleENFRPromptFormatter()
-    prompt = prompt_fmt.format(testset.examples[0])
-
-    output = llm(
-      prompt, # Prompt
-      max_tokens=2, # Generate up to 32 tokens, set to None to generate up to the end of the context window
-      stop=["Q:", "\n"], # Stop generating just before the model would generate a new question
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.float16
     )
 
-    print(output)
+    device = "cuda"
+    model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1", quantization_config=quantization_config)
+    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
+
+    trues = []
+    preds = []
+    for example in tqdm(testset.examples):
+        true, prompt = example.to_prompt(shuffle=False)
+        trues.append(true)
+
+        message = [{"role": "user", "content" : prompt}]
+        inputs = tokenizer.apply_chat_template(message, return_tensors="pt").to(device)
+        outputs = model.generate(inputs, max_new_tokens=2)
+
+        pred = tokenizer.batch_decode(outputs[0, inputs.shape[1]:].unsqueeze(0))
+        try:
+            preds.append(int("".join(filter(str.isdigit, pred))))
+        except ValueError:
+            preds.append(0)
+
+    
+    print(f"accuracy: {accuracy_score(trues, preds)}")
+    print(f"recall: {recall_score(trues, preds, average='micro')}")
+    print(f"f1-score: {f1_score(trues, preds, average='micro')}")
 
 def main():
     simple_comparison_mistral()    
