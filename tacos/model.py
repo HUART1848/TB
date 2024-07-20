4712c7b29c1a5b2da2d284707c6bc252769ac8e0
Hugo Huart <hugo.huart@heig-vd.ch>
2024-07-20 19:00:59 +0200

    Factorize model

--- tacos/model.py
+++ tacos/model.py
@@ -3,53 +3,32 @@ import gc
 from llama_cpp import Llama
 
 class Model:
-    def load(self):
-        ...
+    """Interface for generic LLMs"""
+    name: str = ...
 
-    def unload(self):
-        ...
+    def load(self): ...
+    def unload(self): ...
+    def get_completion(self, prompt: str) -> str: ...
 
-    def get_completion(self, prompt: str, max_tokens: int=64) -> str:
-        ...
-    
+class LlamaCPPModel(Model):
+    """Frontend for `llama.cpp`-compatible models"""
+    model_path: str = ...
+    model: None | Llama = ...
 
-class MistralInstructModel(Model):
-    MODEL_PATH = "./models/mistral/mistral-7B-Q4_K_M.gguf"
-    
-    model: Llama = None
-
-
-    def load(self, verbose: bool=False, n_threads=16):
-        self.model = Llama(model_path=self.MODEL_PATH, verbose=verbose, n_threads=n_threads, n_threads_batch=n_threads)
+    def __init__(self, name: str, model_path: str):
+        self.name = name
+        self.model_path = model_path
+        
+    def load(self, verbose: bool=False, n_threads: int=8):
+        self.model = Llama(model_path=self.model_path, verbose=verbose, n_threads=n_threads, n_threads_batch=n_threads)
 
     def unload(self):
         del self.model
         gc.collect()
 
-    def get_completion(self, prompt: str, max_tokens: int=64) -> str:
+    def get_completion(self, prompt: str, max_tokens: int=64, stop: list[str]=[]) -> str:
         if self.model is None:
-            raise RuntimeError("Model is not loaded")
+            raise RuntimeError(f"Model {self.name} at {self.model_path} is not loaded")
         
-        message = [{"role": "user", "content": prompt}]
-        ret = self.model.create_chat_completion(messages=message, max_tokens=max_tokens, stop=["END"])
-        return ret
-
-class Llama3InstructModel(Model):
-    MODEL_PATH = "./models/llama3/llama3-8B-Q4_K_M.gguf"
-    
-    model = None
-
-    def load(self, verbose: bool=False, n_threads=16):
-        self.model = Llama(model_path=self.MODEL_PATH, verbose=verbose, n_threads=n_threads, n_threads_batch=n_threads)
-
-    def unload(self):
-        del self.model
-        gc.collect()
-
-    def get_completion(self, prompt: str, max_tokens: int=64) -> str:
-        if self.model is None:
-            raise RuntimeError("Model is not loaded")
-        
-        message = [{"role": "user", "content": prompt}]
-        ret = self.model.create_chat_completion(messages=message, max_tokens=max_tokens, stop=["END"])
-        return ret
+        messages = [{"role": "user", "content": prompt}]
+        return self.model.create_chat_completion(messages=messages, max_tokens=max_tokens, stop=stop)
